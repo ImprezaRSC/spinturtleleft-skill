44f2f00b53e1b9996d9d3887cb183f692d6cb22c
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-19 18:27:16 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ class Spinturtleleft(MycroftSkill):
     @intent_file_handler('spinturtleleft.intent')
     def handle_spinturtleleft(self, message):
         self.speak_dialog('spinturtleleft')
-        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.75]' '[0.0, 0.0, 1.0]'"
+        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 1.0]' '[0.0, 0.0, 0.5]'"
         subprocess.call([s],shell=True)
 
 def create_skill():
